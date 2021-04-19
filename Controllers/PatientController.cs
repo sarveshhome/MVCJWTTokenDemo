92b6fda56f1ea1e11f102e9a5f8f8ce9e0218266
sarveshhome <sarvesh.carlton@gmail.com>
2021-04-19 19:43:16 +0100

    EFCore

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCJWTTokenDemo.DAL;
+using MVCJWTTokenDemo.Model;
+
+namespace MVCJWTTokenDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientController : ControllerBase
+    {
+        private PatientDbContext _db = null;
+
+        public PatientController(PatientDbContext db,ILogger logger)
+        {
+            _db = db;
+            db.Database.EnsureCreated();
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpPost("/api/patient")]
+        public IActionResult AddPatient(Patient patientObj)
+        {
+
+            _db.Patients.Add(patientObj); //in memory
+            _db.SaveChanges(); //physical commit 
+            return Content("result");
+        }
+    }
+}
